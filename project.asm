ffc2c218a9859b8b185bf467bfa3d2b3ab4ded48
roy860328 <roy1997860328@gmail.com>
2017-05-13 15:31:13 +0800

    主機左右移動功能新增

--- project.asm
+++ project.asm
@@ -1,13 +1,15 @@
 INCLUDE Irvine32.inc
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;主機宣告;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-Naturemain PROTO
-Natureprint PROTO
+NatureMain PROTO
+NaturePrint PROTO
+NatureMove PROTO
 
 NatureSTRUCT STRUCT
 	naturelong BYTE 6
 	x BYTE 5
 	y BYTE 5
+	countnaturex BYTE 0
 	blood BYTE 5
 	picture BYTE "(^..^)",0
 NatureSTRUCT ENDS
@@ -17,7 +19,7 @@ main  EQU start@0 ;
 .data
 mainy BYTE "1            ",0,"2            ",0,"3            ",0,"4            ",0,"5            ",0,"6            ","123",0
 Nature NatureSTRUCT <>
-countnaturex BYTE 1
+
 
 .code
 main PROC
@@ -28,21 +30,16 @@ mov ecx,0
 mov edx,0
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;執行遊戲(迴圈);;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 RUNGAME:
-	cmp eax,84
-	jb eaxless84
-	call clrscr		;清空螢幕
-	mov eax,0
-	mov countnaturex,-1
-	eaxless84:
 
-	INVOKE Naturemain;呼叫 Str_remove
+	INVOKE NatureMain;呼叫 Str_remove
 
-
-	add eax,14							
+				
 	push eax	;時間暫停術
-	mov eax,200
+	mov eax,0
 	call Delay
 	pop eax
+
+	call clrscr		;清空螢幕
 
 	jmp RUNGAME
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;執行遊戲(迴圈);;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -50,18 +47,18 @@ RUNGAME:
 	exit
 main ENDP
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;PROC;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;----------------------------Naturemain
-Naturemain PROC
-	INVOKE Natureprint
 
-	mov bl,Nature.x
-	add bl,countnaturex
-	mov Nature.x,bl
+;----------------------------Naturemain
+
+Naturemain PROC
+	INVOKE NaturePrint
+	INVOKE NatureMove
 
 	ret
 Naturemain ENDP
-;----------------------------Natureprint
-Natureprint PROC
+;----------------------------NaturePrint
+
+NaturePrint PROC USES eax ecx esi edi
 
 	push edx
 	push eax
@@ -100,12 +97,43 @@ Natureprint PROC
 	mov  al , ")"
 	call Writechar
 	;
+	mov  eax, white + ( black*16 )			;設定前景為白色，背景為黑色
+	call SetTextColor
 
 	pop eax
 	pop edx
 
 	ret
-Natureprint ENDP
+NaturePrint ENDP
+
+;----------------------------NatureMove
+
+NatureMove PROC USES eax ebx esi edi
+	
+	call readchar
+	cmp  eax, 4B00h				;左
+	jz   LEFT
+	cmp  eax, 4D00h				;右
+	jz   RIGHT
+	jmp NatureMoveEDN
+
+	LEFT:
+		
+		mov bl,Nature.x
+		mov Nature.countnaturex,-1
+		jmp NatureMoveEDN
+	RIGHT:
+
+		mov bl,Nature.x
+		mov Nature.countnaturex,1
+
+	NatureMoveEDN:
+		add bl,Nature.countnaturex
+		mov Nature.x,bl
+		mov Nature.countnaturex,0
+	ret
+NatureMove ENDP
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;PROC;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 END main
