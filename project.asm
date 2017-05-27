d609a635f79beb539a6621e6c8d0d4cecc66dd34
Janes <jimmy851208@gmail.com>
2017-05-28 00:50:11 +0800

    詮鈞更新：主機受擊顏色改變、地圖天頂設定
    1. 主機受擊顏色改變，被打到會變黃色
    2. 地圖長寬高設定完成！放在.data裡面
    3. 更新血條位置與介面

--- project.asm
+++ project.asm
@@ -1,37 +1,33 @@
 INCLUDE Irvine32.inc
 
-; 地圖結構
-Map STRUCT
-	top		BYTE	0
-	left	BYTE 	0
-	right 	BYTE 	60
-	buttom 	BYTE 	20
-Map ENDS
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;主機宣告;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+MapMain PROTO
+
 NatureMain PROTO
-NaturePrint PROTO
+NaturePrint PROTO,
+	color: DWORD
 NatureMove PROTO
 NatureAttacks PROTO
 NatureBloodLine PROTO,
 	nowBlood: DWORD
 NatureHitted PROTO
 
-NatureBody STRUCT 							; 主戰機的結構
-	naturelong BYTE 6
-	x BYTE 20
-	y BYTE 20
-	countnaturex BYTE 0
-	blood DWORD 5
-	picture BYTE "(^Y^)",0 				; 如果用chcp 65001的話，應該可以用這個(^￥^)
+NatureBody STRUCT 						; 主戰機的結構
+	naturelong 		BYTE 	6
+	x 				BYTE 	50
+	y 				BYTE 	25 			; the same with MapButtom
+	countnaturex 	BYTE 	0
+	blood 			DWORD 	5
+	picture 		BYTE 	"(^Y^)",0 	; 如果用chcp 65001的話，應該可以用這個(^￥^)
+	color			DWORD 	12
 NatureBody ENDS
 
-;韋成改NatureAttackStruct
+;NatureAttackStruct
 NatureAttackStruct STRUCT
 	x BYTE ?
 	y BYTE ?
 	long BYTE 1
 NatureAttackStruct ENDS
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;主機宣告END;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;敵機宣告;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -51,7 +47,7 @@ EnemyAttackStruct ENDS
 
 EnemyBody STRUCT
 	x BYTE ?
-	y BYTE 0
+	y BYTE 3 									; the same with MapTop
 	countnaturex BYTE 0
 	blood BYTE 5
 	picture BYTE "{\V/}",0 						; 敵機圖形
@@ -64,10 +60,25 @@ Emy_num 	= 5
 
 Nature 		NatureBody 			<>
 na_bullet 	NatureAttackStruct 	<>										;子彈設定
-Enemy 		EnemyBody 			Emy_num DUP(<20>,<35>,<28>,<15>,<8>) 	;對Enemy的位置做出始化
-Emy_bullet	EnemyAttackStruct 	Emy_num DUP(<>,<>,<>,<>,<>) 	; 設定敵機子彈(先每一台敵機一枚子彈)
+Enemy 		EnemyBody 			Emy_num DUP(<20>,<34>,<60>,<114>,<50>) 	;對Enemy的位置做出始化，注意：一定要是二的倍數
+Emy_bullet	EnemyAttackStruct 	Emy_num DUP(<>,<>,<>,<>,<>) 			; 設定敵機子彈(先每一台敵機一枚子彈)
 EmyNum 		BYTE	Emy_num												;螢幕上印出的敵機 (包含死亡的)
+;print data
 GameOver 	BYTE	"Game Over!", 0
+NatureLife	BYTE 	"Nature Life: ", 0
+;
+
+;地圖界線設定-------
+MapTop		BYTE	2
+MapRight	BYTE 	114
+MapLeft		BYTE 	2
+MapButtom	BYTE	25
+;Max right : 114
+;Max left : 2
+;Max top : 2
+;Max buttom : 25
+;-------------------
+
 
 .code
 main PROC
@@ -78,7 +89,8 @@ xor ecx, ecx
 xor edx, edx
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;執行遊戲(迴圈);;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 RUNGAME:
-
+	
+	INVOKE MapMain
 	INVOKE EnemyMain;
 	INVOKE NatureMain;
 				
@@ -96,12 +108,30 @@ RUNGAME:
 main ENDP
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;函式;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Set Map;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+MapMain PROC USES ecx edx eax
+	mov eax, 9 + ( black*16 )		;設定前景為淡紅色，背景為黑色
+	call SetTextColor
+	mov eax, '='
+	mov ecx, 118
+	mov dl, 0
+	mov dh, MapTop
+	call Gotoxy
+
+printTopMap:
+	call WriteChar
+	inc dl
+	call Gotoxy
+	loop printTopMap
+	ret
+MapMain ENDP
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;NaturePROC;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;----------------------------Naturemain
 
 Naturemain PROC
-	INVOKE NaturePrint
+	INVOKE NaturePrint, Nature.color
 	INVOKE NatureMove
 	INVOKE NatureBloodLine, Nature.blood
 
@@ -109,16 +139,19 @@ Naturemain PROC
 Naturemain ENDP
 ;----------------------------NaturePrint
 
-NaturePrint PROC USES eax edx
+NaturePrint PROC USES eax edx,
+	color: DWORD
 	mov dl, Nature.x
 	add dl, -2 							;重新設定主機位置，中間位置在'Y'的部分
 	mov dh, Nature.y
-	mov  eax, 12 + ( black*16 )			;設定前景為淡紅色，背景為黑色
+	mov eax, color + ( black*16 )		;設定前景為淡紅色，背景為黑色
 	call SetTextColor
 	;
 	call Gotoxy
 	mov edx, OFFSET Nature.picture
 	call WriteString
+	mov edx, 12
+	mov Nature.color, edx
 
 	ret
 NaturePrint ENDP
@@ -167,22 +200,25 @@ NatureBloodLine PROC USES ecx eax edx,
 	nowBlood: DWORD
 	mov ecx, nowBlood
 	mov Nature.blood, ecx
-	;cmp ecx, 0
-	;jz gameoverL
-	mov dl, 50		; col
+	mov dl, 100		; col
 	mov dh, 0 		; row
 	call Gotoxy
-	;jmp bloodLoop
-	;gameoverL: 								; 若血條沒有了，就印出GameOver吧
-	;	mov  eax, 10 + ( black*16 )			;設定前景為淡綠色，背景為黑色
-	;	call SetTextColor
-	;	movzx edx, GameOver
-	;	call WriteString
-	;	jmp endOfBlood
-	bloodLoop:
-		mov  eax, 12 + ( black*16 )			;設定前景為淡紅色，背景為黑色
+	cmp ecx, 0 								; 如果寫血條沒有了，就跳過去gameoverL印出
+	jle gameoverL
+
+	mov  eax, 12 + ( black*16 )			;設定前景為淡紅色，背景為黑色
+	call SetTextColor
+	mov edx, OFFSET NatureLife
+	call WriteString
+	jmp bloodLoop
+	gameoverL: 								; 若血條沒有了，就印出GameOver吧
+		mov  eax, 13 + ( black*16 )			;設定前景為淡洋紅色，背景為黑色
 		call SetTextColor
-		mov al, 'a' 							; 印出愛心形狀
+		mov edx, OFFSET GameOver
+		call WriteString
+		jmp endOfBlood
+	bloodLoop:
+		mov al, 'a' 						; 印出愛心形狀
 		call WriteChar
 		inc dh
 		loop bloodLoop
@@ -220,7 +256,7 @@ EnemyPrint PROC USES edi edx eax,
 	mov edi, nowEnemy						;不這樣做直接放EnemyBody PTR Enemy[nowEnemynumber]會爆掉
 	mov dl,((EnemyBody PTR Enemy[edi]).x) 	; 敵機的位置(編號edi的敵機)
 	add dl, -2								; -2 為了調整圖片到圖片的中央
-	mov dh, 0 								; row 
+	mov dh,((EnemyBody PTR Enemy[edi]).y) 	; row 
 	call Gotoxy
 	mov  eax, 10 + ( black*16 )				;設定前景為淡綠色，背景為黑色
 	call SetTextColor
@@ -263,9 +299,12 @@ EnemyAttacks PROC USES edi eax ebx ecx edx,
 		mov ecx, Nature.blood
 		dec ecx
 		INVOKE NatureBloodLine, ecx
+		mov ecx, 14
+		mov Nature.color, ecx
 	.ENDIF
+
 	; 子彈到底的判定，就要再重新打一次
-	cmp dh, 20
+	cmp dh, MapButtom
 	jz resetXY
 	call Gotoxy
 	jmp finalPrint
