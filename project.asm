9d09f1c490f0ede2a1392ae7c02b5a6144aafbf0
Janes <jimmy851208@gmail.com>
2017-06-01 01:50:39 +0800

    重大更新: 宣告獨立，更新合併所有人的code，也請大家看"待完成.txt"
    更新:
    1. 宣告獨立出去，要找宣告要去外面找.inc檔
    2. 合併康捷選單，(選擇鍵是Backspace)
    3. 新增待完成檔案，大家記得要看裡面的內容，也可以為自己增添內容
    4. 成功完成小黑窗邊框設定
    5. 大家加油！

--- project.asm
+++ project.asm
@@ -1,68 +1,41 @@
+TITLE assembly language final project NatureWar
+;----------------------------------------------------------
+;assembly language, the 22nd
+;title: NatureWar
+;author: 蔡政育、高康捷、沈詮鈞、葉韋成
+;update: 2017/06/01
+;----------------------------------------------------------
+
 INCLUDE Irvine32.inc
+INCLUDE MainDec.inc
+INCLUDE NatureDec.inc 		; the declaration of Nature
+INCLUDE EnemyDec.inc 		; the declaration of Enemy
+INCLUDE MenuDec.inc
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;主機宣告;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-MapMain PROTO
-
-NatureMain PROTO
-NaturePrint PROTO,
-	color: DWORD
-NatureMove PROTO
-NatureAttacks PROTO
-NatureBloodLine PROTO,
-	nowBlood: DWORD
-NatureHitted PROTO
-
-NatureBody STRUCT 						; 主戰機的結構
-	naturelong 		BYTE 	6
-	x 				BYTE 	50
-	y 				BYTE 	25 			; the same with MapButtom
-	countnaturex 	BYTE 	0
-	blood 			DWORD 	5
-	picture 		BYTE 	"(^Y^)",0 	; 如果用chcp 65001的話，應該可以用這個(^￥^)
-	color			DWORD 	12
-NatureBody ENDS
-
-;NatureAttackStruct
-NatureAttackStruct STRUCT
-	x BYTE ?
-	y BYTE ?
-	long BYTE 1
-NatureAttackStruct ENDS
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;主機宣告END;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;敵機宣告;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-EnemyMain PROTO
-EnemyPrint PROTO,
-	nowEnemy:DWORD
-EnemyMove PROTO
-EnemyAttacks PROTO,
-	nowEnemy:DWORD
-
-EnemyAttackStruct STRUCT
-	x 		BYTE ?
-	y 		BYTE 0
-	long 	BYTE 1
-	pic		BYTE "v", 0
-EnemyAttackStruct ENDS
-
-EnemyBody STRUCT
-	x BYTE ?
-	y BYTE 3 									; the same with MapTop
-	countnaturex BYTE 0
-	blood BYTE 5
-	picture BYTE "{\V/}",0 						; 敵機圖形
-EnemyBody ENDS
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;敵機宣告END;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 main  EQU start@0 ;
 Emy_num 	= 5
+;++++++++++++++++++ data segment ++++++++++++++++++
 .data
+
+; Menu setting
+Menucontrol Menustr <>
+start byte "開始遊戲",0
+button byte "操控說明",0
+story byte "遊戲故事",0
+set byte "遊戲設定",0
+exitt byte "離開遊戲",0
+arrowhead byte "->",0
+clearup byte "   ",0
+
 
 Nature 		NatureBody 			<>
 na_bullet 	NatureAttackStruct 	<>										;子彈設定
 Enemy 		EnemyBody 			Emy_num DUP(<20>,<34>,<60>,<114>,<50>) 	;對Enemy的位置做出始化，注意：一定要是二的倍數
 Emy_bullet	EnemyAttackStruct 	Emy_num DUP(<>,<>,<>,<>,<>) 			; 設定敵機子彈(先每一台敵機一枚子彈)
-EmyNum 		BYTE	Emy_num												;螢幕上印出的敵機 (包含死亡的)
+EmyNum 		BYTE	Emy_num												; 敵機數量												;螢幕上印出的敵機 (包含死亡的)
+randVal     DWORD   ?
+mapSize 	COORD 				<120, 30> 								; 視窗畫面設定尺寸
+outputHandle DWORD				0										; 設定輸出型態
 ;print data
 GameOver 	BYTE	"Game Over!", 0
 NatureLife	BYTE 	"Nature Life: ", 0
@@ -79,7 +52,7 @@ MapButtom	BYTE	25
 ;Max buttom : 25
 ;-------------------
 
-
+;++++++++++++++++++ code segment ++++++++++++++++++
 .code
 main PROC
 
@@ -87,6 +60,70 @@ xor eax, eax 				; set register 0
 xor ebx, ebx
 xor ecx, ecx
 xor edx, edx
+
+push eax
+INVOKE GetStdHandle, STD_OUTPUT_HANDLE      ;Get the console output handle
+mov outputHandle, eax                       ;save console handle
+pop eax
+
+INVOKE SetConsoleScreenBufferSize, 			; 設定視窗邊界
+	outputHandle,
+	mapSize 								; 視窗邊界跟這個變數有關
+
+cmp Menucontrol.start, 1 					; 若選單選擇"遊戲開始"，進入遊戲
+jz RUNGAME
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;標題畫面;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+Titleset:
+    mov dl, Menucontrol.x
+    mov dh, Menucontrol.y
+    mov  eax, 12 + ( black*16 )
+	call SetTextColor
+	call Gotoxy
+	mov edx, offset arrowhead
+	call WriteString
+
+	mov dh, 2
+	mov dl, 10
+	call Gotoxy
+	mov  eax, 15 + ( black*16 )
+	call SetTextColor
+	mov edx, offset start
+	call WriteString
+
+	mov dh, 4
+	mov dl, 10
+	call Gotoxy
+    mov edx, offset button
+	call WriteString
+
+	mov dh, 6
+	mov dl, 10
+	call Gotoxy
+    mov edx, offset story
+	call WriteString
+
+	mov dh, 8
+	mov dl, 10
+	call Gotoxy
+    mov edx, offset set
+	call WriteString
+
+	mov dh, 10
+	mov dl, 10
+	call Gotoxy
+    mov edx, offset exitt
+	call WriteString
+	call Crlf
+    mov Menucontrol.back, 0
+
+GameTitle:
+
+    invoke Menu
+    cmp Menucontrol.back, 1
+    jz Titleset
+	jmp GameTitle
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;執行遊戲(迴圈);;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 RUNGAME:
 	
@@ -108,6 +145,138 @@ RUNGAME:
 main ENDP
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;函式;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;選單;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+Menu proc
+
+    invoke Menumove
+    invoke Menuprint
+
+    ret
+Menu endp
+Menuprint proc uses eax ebx ecx edx
+
+    mov dl, 7
+    mov dh, Menucontrol.y
+    mov  eax, 12 + ( black*16 )
+	call SetTextColor
+	call Gotoxy
+	mov edx, offset arrowhead
+	call WriteString
+
+    cmp Menucontrol.save, 0
+    jz L1
+    mov dl, 7
+    mov dh, Menucontrol.save
+    add dh, Menucontrol.y
+    mov  eax, 12 + ( black*16 )
+	call SetTextColor
+	call Gotoxy
+	mov edx, offset clearup
+	mov Menucontrol.save, 0
+	call WriteString
+L1:
+    ret
+Menuprint endp
+
+Menumove proc uses eax ebx ecx edx
+
+    call readchar
+
+	cmp  eax, 4800h				;上
+	jz   UP
+	cmp  eax, 5000h				;下
+	jz   DOWN
+	cmp  eax, 0E08h				;退格鍵
+	jz	 SPACE
+	jmp MenumoveEND
+
+	SPACE:
+        mov bl, Menucontrol.y
+
+        cmp bl, 2
+        jz startL
+        cmp bl, 4
+        jz buttonL
+        cmp bl, 6
+        jz storyL
+        cmp bl, 8
+        jz setLL
+        invoke ExitProcess,0
+    startL:
+        invoke startproc
+        mov Menucontrol.back, 1
+        jmp ED
+    buttonL:
+        invoke buttonproc
+        mov Menucontrol.back, 1
+        jmp ED
+    storyL:
+        invoke storyproc
+        mov Menucontrol.back, 1
+        jmp ED
+    setLL:
+        invoke setproc
+        mov Menucontrol.back, 1
+        jmp ED
+
+	UP:
+        mov bl, Menucontrol.y
+        cmp Menucontrol.y, 2
+        jz MenumoveEND
+        mov Menucontrol.ct, -2
+        mov Menucontrol.save, 2
+		jmp MenumoveEND
+
+	DOWN:
+
+        mov bl, Menucontrol.y
+        cmp Menucontrol.y, 10
+        jz MenumoveEND
+        mov Menucontrol.ct, 2
+        mov Menucontrol.save, -2
+		jmp MenumoveEND
+
+
+	MenumoveEND:
+		add bl, Menucontrol.ct
+		mov Menucontrol.y, bl
+		mov Menucontrol.ct, 0
+    ED:
+	ret
+Menumove ENDP
+startproc proc uses eax ebx ecx edx
+    call Clrscr
+
+    call WaitMsg
+    call Clrscr
+    mov Menucontrol.start, 1
+    invoke main
+    ret
+startproc endp
+
+buttonproc proc uses eax ebx ecx edx
+    call Clrscr
+
+    call WaitMsg
+    call Clrscr
+    ret
+buttonproc endp
+
+storyproc proc uses eax ebx ecx edx
+    call Clrscr
+
+    call WaitMsg
+    call Clrscr
+    ret
+storyproc endp
+
+setproc proc uses eax ebx ecx edx
+    call Clrscr
+
+    call WaitMsg
+    call Clrscr
+    ret
+setproc endp
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Set Map;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 MapMain PROC USES ecx edx eax
 	mov eax, 9 + ( black*16 )		;設定前景為淡紅色，背景為黑色
@@ -166,9 +335,15 @@ NatureMove PROC USES eax ebx
 	jz   RIGHT
 	cmp  eax, 0020h				; 空白鍵
 	jz	 SPACE
+	cmp  eax, 2C7Ah             ; 攻擊鍵Z
+	jz   ATTACK
 
 	jmp DontMove
 
+	ATTACK:
+        mov al, 1
+        mov na_bullet.exist, al
+        mov al, 0
 	SPACE:
 		INVOKE NatureAttacks
 		jmp NatureMoveEDN
@@ -190,8 +365,41 @@ NatureMove PROC USES eax ebx
 NatureMove ENDP
 ;----------------------------NatureAttack
 
-NatureAttacks PROC USES eax ebx
+NatureAttacks PROC USES esi edi eax ebx ecx edx
+    mov edi, 0
+    mov al, na_bullet.x
+    .IF(al == 0)
+        mov al, Nature.x
+    .ENDIF
+    mov dl, al
 
+    mov ah, na_bullet.y
+    .IF(ah == 25)
+        mov ah, Nature.y
+    .ENDIF
+    mov dh, ah
+
+    mov na_bullet.x, al
+    dec dh
+    mov na_bullet.y, dh
+
+    cmp dh, MapTop
+    jz ResetBullet
+    call GotoXY
+    jmp PrintBullet
+    ResetBullet:
+        mov dl, 0
+        mov na_bullet.x, dl
+        mov al, 0
+        mov dh, 25
+        mov na_bullet.y, dh
+        mov ah, 0
+        mov na_bullet.exist, ah
+    PrintBullet:
+        mov al, '^'
+        call WriteChar
+
+    NatureAttacksEND:
 	ret
 NatureAttacks ENDP
 ;----------------------------NatureBloodLine
@@ -240,7 +448,19 @@ Enemymain PROC USES ecx esi edi
 	xor edi, edi 							; edi指的是敵機子彈的編號，從零開始
 	PrintEnemy:								;****迴圈 印出多個敵人****
 		INVOKE EnemyPrint , esi
-		INVOKE EnemyMove
+		push ecx
+		push esi
+		push edi
+		push edx
+		push ebx
+		push eax
+		INVOKE EnemyMove, esi
+		pop eax
+		pop ebx
+		pop edx
+		pop edi
+		pop esi
+		pop ecx
 
 		INVOKE EnemyAttacks , edi 			; print敵機的子彈
 		
@@ -267,7 +487,75 @@ EnemyPrint PROC USES edi edx eax,
 EnemyPrint ENDP
 
 ;----------------------------EnemyMove
-EnemyMove PROC 
+EnemyMove PROC USES edi eax ebx ecx edx esi,
+	nowEnemy:DWORD
+
+	mov edi,nowEnemy
+	movzx ecx, EmyNum
+
+	mov eax,4				;;隨機-1~1數字 (讓敵機移動)
+	call RandomRange
+	mov randVal,eax
+
+	.IF (eax == 2)
+	 mov randVal,-1
+	.ENDIF
+	.IF (eax == 3)
+	 mov randVal,0
+	.ENDIF
+
+	movzx eax,((EnemyBody PTR Enemy[edi]).x)	;;讓敵機位置加上x
+	add eax,randVal
+	mov esi,0
+
+
+	testrange:				;;測試如果加了變數的x是否會和其他敵機重疊
+	push eax
+	movzx ebx,((EnemyBody PTR Enemy[esi]).x)
+	.IF (edi == esi)
+	 jmp goahead
+	.ENDIF
+	;互減來判斷距離
+	neg ebx
+	add eax,ebx
+	mov edx,5
+
+	cmp eax,edx				;;比較距離是否在-5~5之間 如果是則不能移動 跳到cantmove
+	JG  goahead
+	neg edx
+	cmp eax,edx
+	JG  cantmove
+
+	goahead:
+	pop eax
+	add esi, TYPE Enemy
+	loop testrange			;;結束testrange測試
+
+
+
+	.IF (eax <= 1)			;;先判斷是否有超出界線 如果沒有則可以設定敵機新位置
+	 jmp cantmove
+	.ENDIF
+	.IF (eax >= 40)
+	 jmp cantmove
+	.ENDIF
+	mov ((EnemyBody PTR Enemy[edi]).x),al
+
+	cantmove:
+
+	mov eax,10				;;設定敵機的y是否要往下移動  給定隨機變數0~9 如果等於2則敵機會往下移動
+	call RandomRange
+	mov randVal,eax
+	mov randVal,0
+
+	.IF (eax == 2)
+	 mov randVal,1
+	.ENDIF
+	;;
+	mov eax,0
+	mov al,((EnemyBody PTR Enemy[edi]).y)
+	add eax,randVal
+	mov ((EnemyBody PTR Enemy[edi]).y),al
 
 	ret
 EnemyMove ENDP
